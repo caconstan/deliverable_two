b6ef815737c5ad4ba953b85a02cc5c992f826dc7
chris <caconstan@yahoo.com>
2020-04-16 12:37:31 -0400

    First commit for deliverable two

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your message?");
+            string input = Console.ReadLine();
+            input = input.ToUpper();
+            string message = null;
+            int checksum = 0;
+
+            foreach (char c in input.ToCharArray())
+            {
+                checksum += c;
+                message += (c - 64) + "-";
+            }
+
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " + checksum);
+        }
+    }
+}
